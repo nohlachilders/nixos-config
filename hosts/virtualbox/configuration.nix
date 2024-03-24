d0e294510a2e8bb389461cf23beabd1a53a0cc86
Nohl Childers <nohlachilders@gmail.com>
2024-03-24 16:36:39 -0700

    brackets and semicolon

--- hosts/virtualbox/configuration.nix
+++ hosts/virtualbox/configuration.nix
@@ -18,11 +18,9 @@
     # FIXME: Replace with your username
     nohlachilders = {
       isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["networkmanager" "wheel" ];
+    };
   };
 
 
