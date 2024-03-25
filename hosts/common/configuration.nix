d47162de2dc8406072d2dbf752d28cc826fc4cb0
Nohl Childers <nohlachilders@gmail.com>
2024-03-25 15:57:39 -0700

    this is not a home manager option

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -28,6 +28,8 @@
     ripgrep
   ];
 
+  pkgs.shell = zsh;
+
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
   nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
