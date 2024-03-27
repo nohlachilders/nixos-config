f4ed53d5fc0694e7027c2dcef9638affe4cdb654
Nohl Childers <nohlachilders@gmail.com>
2024-03-27 00:21:15 -0700

    caching for the hyprland flake

--- modules/home-manager/common.nix
+++ modules/home-manager/common.nix
@@ -18,6 +18,11 @@
     stateVersion = "23.05";
   };
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   nixpkgs = {
     # You can add overlays here
     overlays = [
