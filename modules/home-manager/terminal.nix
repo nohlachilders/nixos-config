81519edc7a64aa787772cb344910d9a28743c0c9
Nohl Childers <nohlachilders@gmail.com>
2024-03-31 20:21:19 -0700

    now lets watch this not work lol

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -18,6 +18,28 @@ programs = {
       export VISUAL=nvim
       # Remove mode switching delay.
       KEYTIMEOUT=10
+      # Change cursor shape for different vi modes.
+      function zle-keymap-select {
+      if [[ ${KEYMAP} == vicmd ]] ||
+        [[ $1 = 'block' ]]; then
+            echo -ne '\e[1 q'
+
+        elif [[ ${KEYMAP} == main ]] ||
+            [[ ${KEYMAP} == viins ]] ||
+            [[ ${KEYMAP} = ' ' ]] ||
+            [[ $1 = 'beam' ]]; then
+                    echo -ne '\e[5 q'
+     fi
+      }
+     zle -N zle-keymap-select
+
+     # Use beam shape cursor on startup.
+     echo -ne '\e[5 q'
+     _fix_cursor() {
+         echo -ne '\e[5 q'
+     }
+
+     precmd_functions+=(_fix_cursor)
     '';
   };
 
