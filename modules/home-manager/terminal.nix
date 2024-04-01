682348797aa1b275b905ff92c9b280f2c9aedfa5
Nohl Childers <nohlachilders@gmail.com>
2024-03-31 20:25:42 -0700

    did i escape properly?

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -20,14 +20,14 @@ programs = {
       KEYTIMEOUT=10
       # Change cursor shape for different vi modes.
       function zle-keymap-select {
-      if [[ ${KEYMAP} == vicmd ]] ||
+      if [[ ''${KEYMAP} == vicmd ]] ||
         [[ $1 = 'block' ]]; then
             echo -ne '\e[1 q'
 
-        elif [[ ${KEYMAP} == main ]] ||
-            [[ ${KEYMAP} == viins ]] ||
-            [[ ${KEYMAP} = ' ' ]] ||
-            [[ $1 = 'beam' ]]; then
+        elif [[ ''${KEYMAP} == main ]] ||
+            [[ ''${KEYMAP} == viins ]] ||
+            [[ ''${KEYMAP} = ''\'''\' ]] ||
+            [[ ''$1 = 'beam' ]]; then
                     echo -ne '\e[5 q'
      fi
       }
