d1ceda0802efe78bd3fced2a636baf040eea3b89
Nohl Childers <nohlachilders@gmail.com>
2024-04-02 13:36:22 -0700

    again

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -32,8 +32,8 @@ programs = {
     initExtra = ''
       bindkey -v
       bindkey -M viins jk vi-cmd-mode
-      bindkey -M viins ^P vi-history-search-backward
-      bindkey -M viins ^N vi-history-search-forward
+      bindkey -M viins "^P" vi-history-search-backward
+      bindkey -M viins "^N" vi-history-search-forward
       export VISUAL=nvim
       # Remove mode switching delay.
       KEYTIMEOUT=10
