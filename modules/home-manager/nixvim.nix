67bda75cc844c32647006934df7fffe54bab3a74
Nohl Childers <nohlachilders@gmail.com>
2024-04-02 12:36:59 -0700

    rest of the default nvim settings

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -101,6 +101,31 @@
       vim.keymap.set("n", "Q", "<nop>")
       vim.keymap.set("n", "<leader>w", vim.cmd.w)
       vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
+
+      vim.opt.nu = true
+      vim.opt.relativenumber = true
+
+      vim.opt.tabstop = 4
+      vim.opt.softtabstop = 4
+      vim.opt.shiftwidth = 4
+      vim.opt.expandtab = true
+      vim.opt.wrap = false
+      vim.opt.hlsearch = false
+      vim.opt.incsearch = true
+      vim.opt.termguicolors = true
+      vim.opt.scrolloff = 8
+      vim.opt.signcolumn = "yes"
+      vim.opt.isfname:append("@-@")
+      vim.opt.updatetime = 50
+      vim.opt.colorcolumn = "80"
+
+      function fix_transparency()
+        vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+        vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+      end
+
+      fix_transparency()
+
     '';
-      };
-    }
+  };
+}
