cd781218a5b025f48bc6126336cb9495945eb6b7
Nohl Childers <nohlachilders@gmail.com>
2024-04-01 22:35:16 -0700

    settings

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -23,15 +23,40 @@
           nil_ls.enable = true;
         };
       };
+
+      treesitter.enable = true;
+
+      telescope = {
+        enable = true;
+        keymaps = {
+          "<leader>f" = "find_files";
+          "<leader>g" "live_grep";
+        };
+      };
     };
+
+    keymaps = [
+      #{}
+    ];
 
     colorschemes.gruvbox.enable = true;
 
     globals.mapleader = " ";
 
     extraConfigLua = ''
-      -- Print a little welcome message when nvim is opened!
-      print("Hello world!")
+      vim.keymap.set("i","jk", "<Esc>")
+      vim.keymap.set("v","jk", "<Esc>")
+      vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+      vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+      vim.keymap.set("n", "J", "mzJ`z")
+      vim.keymap.set("n", "<C-d>", "<C-d>zz")
+      vim.keymap.set("n", "<C-u>", "<C-u>zz")
+      vim.keymap.set("n", "n", "nzzzv")
+      vim.keymap.set("n", "N", "Nzzzv")
+      vim.keymap.set("x", "<leader>p", "\"_dP")
+      vim.keymap.set("n", "Q", "<nop>")
+      vim.keymap.set("n", "<leader>w", vim.cmd.w)
+      vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
     '';
   };
 
