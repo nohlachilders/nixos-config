bb9b21469f16a77c31474421f0285ea062dbf965
Nohl Childers <nohlachilders@github.com>
2024-04-09 01:37:04 -0700

    this change didnt work lol

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -12,6 +12,7 @@
         enable = true;
         package = pkgs.unstable.emacs;
         extraConfig = ''
+            (setq inhibit-startup-screen t)
             (load-file "~/Projects/nohl-emacs-files/dotfiles/init.el")
             '';
         extraPackages = epkgs: with epkgs; [
