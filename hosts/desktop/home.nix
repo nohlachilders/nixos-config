0e772800164e487009f8f8497b560958b3ffacfe
Nohl Childers <nohlachilders@github.com>
2024-04-18 10:27:24 -0700

    monitors

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -17,8 +17,8 @@ let
     '';
     wallpaperScript = pkgs.pkgs.writeShellScriptBin "wallpaper" ''
         swww init
-        swww img -o VGA-1 ~/Dropbox/drawings/048.png
-        swww img -o DP-3 ~/Dropbox/drawings/face2.png
+        swww img -o DP-3 ~/Dropbox/drawings/048.png
+        swww img -o DP-1 ~/Dropbox/drawings/face2.png
         swww img -o VGA-1 ~/Dropbox/drawings/047_3.png
     '';
 in {
@@ -35,9 +35,9 @@ in {
                 ''${workspaceScript}/bin/workspace''
             ]; 
             monitor = [
-                "DP-3,1920x1080,0x0,1"
+                "DP-1,1920x1080@120,0x0,1"
                 "HDMI-A-1,1920x1080, 0x1080, 1, transform, 2" 
-                "VGA-1,1440x900, 1920x-360, 1, transform, 1"
+                "DP-3,1440x900, 1920x-360, 1, transform, 1"
             ];
             windowrule = [
                 "float,title:^(Picture-in-Picture)$"
@@ -45,16 +45,16 @@ in {
             ];
             workspace = [
                 #"name:1, monitor:DP-3, default:true"
-                "name:2, monitor:DP-3"
-                "name:3, monitor:DP-3"
-                "name:4, monitor:DP-3"
-                "name:5, monitor:DP-3"
-                "name:6, monitor:DP-3"
-                "name:7, monitor:DP-3"
-                "name:8, monitor:DP-3"
-                "name:9, monitor:DP-3"
-                "name:10, monitor:DP-3"
-                "name:side, monitor:VGA-1, default:true"
+                "name:2, monitor:DP-1"
+                "name:3, monitor:DP-1"
+                "name:4, monitor:DP-1"
+                "name:5, monitor:DP-1"
+                "name:6, monitor:DP-1"
+                "name:7, monitor:DP-1"
+                "name:8, monitor:DP-1"
+                "name:9, monitor:DP-1"
+                "name:10, monitor:DP-1"
+                "name:side, monitor:DP-3, default:true"
                 "name:cintiq, monitor:HDMI-A-1, default:true"
             ];
         };
