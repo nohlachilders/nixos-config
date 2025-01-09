b0a502d9d0cb2acd7eda52a73a3bb6de56433ba0
Nohl Childers <nohlachilders@github.com>
2025-01-08 18:27:49 -0800

    rewrote the live symlinks to make them slightly more robust

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -12,9 +12,9 @@ imports = [
 
   home.file = {
     ## these are loaded from in the local repo, and can be live edited without reloading the system
-    ".config/nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink /home/nohlachilders/Projects/nixos-config/dotfiles/.config/nvim/init.lua;
-    ".zshrc".source = config.lib.file.mkOutOfStoreSymlink /home/nohlachilders/Projects/nixos-config/dotfiles/.zshrc;
-    ".config/tmux/tmux.conf".source = config.lib.file.mkOutOfStoreSymlink /home/nohlachilders/Projects/nixos-config/dotfiles/.config/tmux/tmux.conf;
+    ".config/nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nixos-config/dotfiles/.config/nvim/init.lua";
+    ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nixos-config/dotfiles/.zshrc";
+    ".config/tmux/tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nixos-config/dotfiles/.config/tmux/tmux.conf";
 
     ## these are not, but they dont ever need to be tweaked really
     ".local/share/zsh/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
