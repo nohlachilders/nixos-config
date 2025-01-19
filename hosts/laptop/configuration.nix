8a354e6bd7f4268e60de79c61ace3355f168ba17
Nohl Childers <nohlachilders@github.com>
2025-01-19 02:19:42 -0800

    formatted a file

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,65 +1,65 @@
 # main OS config
 {
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
+inputs,
+outputs,
+lib,
+config,
+pkgs,
+...
 }: {
-  imports = [
-    inputs.home-manager.nixosModules.home-manager
-    #outputs.nixosModules.swayX
-    outputs.nixosModules.common
-    outputs.nixosModules.hyprland-nvidia-compat
-    outputs.nixosModules.emacs
-    outputs.nixosModules.cyber
-    ./hardware-configuration.nix
-  ];
+    imports = [
+        inputs.home-manager.nixosModules.home-manager
+        #outputs.nixosModules.swayX
+        outputs.nixosModules.common
+        outputs.nixosModules.hyprland-nvidia-compat
+        outputs.nixosModules.emacs
+        outputs.nixosModules.cyber
+        ./hardware-configuration.nix
+    ];
 
-  environment.systemPackages = with pkgs; [
-    neofetch
+    environment.systemPackages = with pkgs; [
+        neofetch
 
-            unstable.hyprland
-            unstable.wofi
-            unstable.mako
-            unstable.libnotify
-            unstable.swww
-            unstable.wl-clipboard
-            unstable.cliphist
-            unstable.grim
-            unstable.slurp
-            unstable.grimblast
-            unstable.kitty
+        unstable.hyprland
+        unstable.wofi
+        unstable.mako
+        unstable.libnotify
+        unstable.swww
+        unstable.wl-clipboard
+        unstable.cliphist
+        unstable.grim
+        unstable.slurp
+        unstable.grimblast
+        unstable.kitty
 
-    maestral
-    zathura
+        maestral
+        zathura
 
-    discord
+        discord
 
-    zap
+        zap
 
-    acpi
-  ];
+        acpi
+    ];
 
-fonts.packages = with pkgs;[
-    overpass
-    source-sans-pro
-    helvetica-neue-lt-std
-    fira-code
-    fira-code-nerdfont
-    font-awesome
-];
+    fonts.packages = with pkgs;[
+        overpass
+        source-sans-pro
+        helvetica-neue-lt-std
+        fira-code
+        fira-code-nerdfont
+        font-awesome
+    ];
 
 
-  boot.loader.grub= {
-    enable = true;
-    useOSProber = true;
-  };
+    boot.loader.grub= {
+        enable = true;
+        useOSProber = true;
+    };
 
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.efiSupport = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
 
     services.xserver.enable = true;
@@ -70,30 +70,30 @@ fonts.packages = with pkgs;[
         xwayland.enable = true;
     };
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
-xdg.portal.enable = true;
-xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
-xdg.portal.config.common.default = "*";
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    xdg.portal.config.common.default = "*";
 
-  networking.hostName = "nixos";
+    networking.hostName = "nixos";
 
-  home-manager = {
-    users = {
-      # Import your home-manager configuration
-      nohlachilders = import ./home.nix;
+    home-manager = {
+        users = {
+            # Import your home-manager configuration
+            nohlachilders = import ./home.nix;
+        };
     };
-  };
 
-  users.users = {
-    # FIXME: Replace with your username
-    nohlachilders = {
-      isNormalUser = true;
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["networkmanager" "wheel" "audio" ];
+    users.users = {
+        # FIXME: Replace with your username
+        nohlachilders = {
+            isNormalUser = true;
+            # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+            extraGroups = ["networkmanager" "wheel" "audio" ];
 
-      shell = pkgs.zsh;
+            shell = pkgs.zsh;
+        };
     };
-  };
-  
+
 
 
 }
